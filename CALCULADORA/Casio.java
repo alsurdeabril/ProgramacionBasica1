10e522f99497042f90502bed0e97faf8d13d1877
Sabrina <66120618+alsurdeabril@users.noreply.github.com>
2020-10-01 16:33:43 -0300

    Aplicación del método set

--- CALCULADORA/Casio.java
+++ CALCULADORA/Casio.java
@@ -4,18 +4,18 @@ public class Casio {
 
 	public static void main(String[] args) {
 		
-		Calculadora casio = new Calculadora(); // no recibe par·metros
+		Calculadora casio = new Calculadora(); // no recibe par√°metros
 		
-	
+		casio.setOperadores(10, 5); // 
 	
 		
 		System.out.println("El resultado de la suma es : " + casio.sumar());
 		
 		System.out.println("El resultado de la resta es: " + casio.restar());
 		
-		System.out.println("El resultado de la multiplicaciÛn es: " + casio.multiplicar());
+		System.out.println("El resultado de la multiplicaci√≥n es: " + casio.multiplicar());
 		
-		System.out.println("El resultado de la divisiÛn es: " + casio.dividir());
+		System.out.println("El resultado de la divisi√≥n es: " + casio.dividir());
 	} // end main
 
 } // end Casio class
