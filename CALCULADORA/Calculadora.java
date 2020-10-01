74140696584efac802c38f9a87f3791b41cae27e
Sabrina <66120618+alsurdeabril@users.noreply.github.com>
2020-10-01 16:38:49 -0300

    Modificación en método set

--- CALCULADORA/Calculadora.java
+++ CALCULADORA/Calculadora.java
@@ -5,75 +5,78 @@ public class Calculadora {
 	/* Atributos propios de la clase que reflejan el estado de la clase Calculadora2 */
 	private double operador1;
 	private double operador2;
-	private double suma;
-	private double resta;
-	private double multiplicacion;
-	private double division;
 	
-	/* Constructor: formato similar al de un m閠odo pero NO va a tener valor de retorno
+	
+	/* Constructor: formato similar al de un m茅todo pero NO va a tener valor de retorno
 	 * En el constructor le asignamos valores a los atributos de ese objeto y los inicializamos */
 	public Calculadora() {
-		this.operador1 = 10.0;
-		this.operador2 = 5.0;
-		this.suma = 0.0;
-		this.resta = 0.0;
-		this.multiplicacion = 0.0;
-		this.division = 0.0;
+		this.operador1 = 0.0;
+		this.operador2 = 0.0;
+		
 		
 	} // end Calculadora constructor 1
 	
 	public Calculadora(double operador1, double operador2) {
 		this.operador1 = operador1;
 		this.operador2 = operador2;
-		this.suma = 0.0;
-		this.resta = 0.0;
-		this.multiplicacion = 0.0;
-		this.division = 0.0;
+		
 	
 	} //  end Calculadora constructor 2
 	
-	/* M閠odos: van a describir el comportamiento de la clase de tipo Televisor */
+	/* M茅todos: van a describir el comportamiento de la clase de tipo Televisor */
 	public double sumar() { // double tipo de dato que devuelve la variable de retorno
-		suma =  this.operador1 + this.operador2; // operaci髇 que va a devolver el m閠odo
+		double suma = 0.0;
+		suma =  this.operador1 + this.operador2; // operaci贸n que va a devolver el m茅todo
 		return suma;
 	} // end sumar 1
 	
 	public double sumar(double operador1, double operador2) { //double tipo de dato variable de retorno
-		suma = this.operador1 + this.operador2;  // operaci髇 que va a devolver el m閠odo
+		double suma = 0.0;
+		suma = this.operador1 + this.operador2;  // operaci贸n que va a devolver el m茅todo
 		return suma;
 	} // end suma 2
 	
 	public double restar() { //double tipo de dato variable de retorno
-		resta = this.operador1 - this.operador2;  // operaci髇 que va a devolver el m閠odo
+		double resta = 0.0;
+		resta = this.operador1 - this.operador2;  // operaci贸n que va a devolver el m茅todo
 		return resta;
 	} // end resta 1
 	
 	public double restar(double operador1, double operador2) { // double tipo dato variable retorno
-		resta = this.operador1 - this.operador2; // operaci髇 que va a devolver el m閠odo
+		double resta = 0.0;
+		resta = this.operador1 - this.operador2; // operaci贸n que va a devolver el m茅todo
 		return resta;
 	} // end resta2
 	
 	public double multiplicar() {  //double tipo de dato variable de retorno
-		multiplicacion = this.operador1 * this.operador2; // operaci髇 que va a devolver el m閠odo
+		double multiplicacion = 0.0;
+		multiplicacion = this.operador1 * this.operador2; // operaci贸n que va a devolver el m茅todo
 		return multiplicacion;
 	} // end multiplicar1
 	
 	public double multiplicar(double operador1, double operador2) {  //double tipo de dato variable de retorno
-		multiplicacion = this.operador1 * this.operador2; // operaci髇 que va a devolver el m閠odo
+		double multiplicacion = 0.0;
+		multiplicacion = this.operador1 * this.operador2; // operaci贸n que va a devolver el m茅todo
 		return multiplicacion;
 	} // end multiplicar2
 	
 	public double dividir() {  //double tipo de dato variable de retorno
+		double division = 0.0;
 		division = this.operador1 / this.operador2;  //double tipo de dato variable de retorno
 		return division;
 	} // end division1
 	
 	public double dividir(double numerador, double divisor) {  //double tipo de dato variable de retorno
+		double division = 0.0;
 		division = this.operador1 / this.operador2;  //double tipo de dato variable de retorno
 		return division;
 	} // end division2
 	
-
+	public void setOperadores(double operador1, double operador2) { //no devuelve el resultado de ninguna acci贸n
+		this.operador1 = operador1; // establecemos los operadores para que el objeto que no recibe
+		this.operador2 = operador2; // par谩metros pueda operar
+	} // end setOperadores -> M茅todo que actualiza el valor de un atributo
+	
 	
 
 } // end Calculadora class
